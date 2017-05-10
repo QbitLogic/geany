b982dc349029b991351c29a8571a624d57126c60
C0deAi <benjamin.bales@assrc.us>
2017-05-10 18:05:12 -0400

    doc->file_type pointer might be dereferenced when null on line 283.  Initialization may be provided by doc pointer passed in as function argument, but a null check would be prudent just in case. The fix checks doc->file_type for null before allowing a dereference on the following line.

--- plugins/saveactions.c
+++ plugins/saveactions.c
@@ -280,8 +280,10 @@ static void instantsave_document_new_cb(GObject *obj, GeanyDocument *doc, gpoint
 
 		doc->file_name = new_filename;
 
-		if (doc->file_type->id == GEANY_FILETYPES_NONE)
+		if(doc->file_type) {
+		    if (doc->file_type->id == GEANY_FILETYPES_NONE)
 			document_set_filetype(doc, filetypes_lookup_by_name(instantsave_default_ft));
+			}
 
 		/* force saving the file to enable all the related actions(tab name, filetype, etc.) */
 		document_save_file(doc, TRUE);
